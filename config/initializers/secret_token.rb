15aa5b4a4446e5353376f830473e5d5cfc9b2525
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2013-10-05 21:47:02 +0400

    Just initial version of Rails 4 applicaiton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shopen::Application.config.secret_key_base = '3ca254dc2d7fc1bfc18f45dd0b04f599121b6bd06ae02bef74f6e931fc5429d274bb65ec685c39a85206773cc3f04da772b522981ee108830719009d1d62251f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shopen::Application.config.session_store :cookie_store, key: '_shopen_session'

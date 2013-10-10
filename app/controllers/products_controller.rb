3e0dfbf9318b43c37aa61c15afc249cc47cbed29
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2013-10-10 19:49:09 +0400

    Product page mockup is finished

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,14 @@ class ProductsController < ApplicationController
   before_action :load_categories
   before_action :load_products
 
+  RELATED_PRODUCTS_COUNT = 10
+  
+  def index
+
+  end
+  
   def show
+    @related_products = Product.all.limit(RELATED_PRODUCTS_COUNT)
     @product = @products.find(params[:id])
   end
 

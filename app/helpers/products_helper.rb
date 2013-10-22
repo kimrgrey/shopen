9b27717567087bb18138b7a0f244b1767b06807d
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2013-10-22 22:34:07 +0400

    Small product grid view fixes. Red ribbon should be dispalyed only for products with discount

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,4 +1,14 @@
-module PriceHelper
+module ProductsHelper
+  DEFAULT_IMAGE_OPTIONS = { :size => :small }
+
+  def product_image(product, options = DEFAULT_IMAGE_OPTIONS)
+    content_tag(:a, content_tag(:img, nil, src: image_url("product-l1.jpg")), href: product_path(product))      
+  end
+
+  def product_sale_ribbon(product)
+    content_tag(:span, "Sale", class: 'sale')
+  end
+
   def product_price(product)
     if product.has_discount?
       [

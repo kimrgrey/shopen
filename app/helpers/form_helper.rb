08d001e7cdd03d94ba4821575cda3926dc01f873
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2013-10-23 18:50:01 +0400

    Internationalization is used for products pages in admin area

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,4 +1,8 @@
 module FormHelper
+  def text_area_rows_count
+    15
+  end
+
   def form_fieldset_header(title, description)
     raw [content_tag(:h3, title), content_tag(:p, description)].join()
   end

ae9e7012de9f5122a558d392c95578e4a43a23a3
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2013-10-24 17:59:48 +0400

    Internationalization on russian added

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Shopen::Application.routes.draw do
-  devise_for :users, controllers: {sessions: 'sessions', registrations: 'registrations'}
-  
-  namespace :admin do 
-    resources :products
-
-    root to: 'products#index'
+  scope "(:locale)", locale: /en|ru/ do
+    devise_for :users, controllers: {sessions: 'sessions', registrations: 'registrations'}
+    
+    namespace :admin do 
+      resources :products
+      root to: 'products#index'
+    end
+    resource :user
+    resources :categories
+    resources :products, only: [:index, :show]
+    get '/:locale' => 'products#index'
   end
-
-  resource :user
-  resources :categories
-  resources :products, only: [:index, :show]
 
   root to: 'products#index'
 end

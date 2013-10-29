22871a2722ec2f3e53e27b01363b451282b32e09
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2013-10-29 16:48:11 +0400

    Language selector is working. Currency selector was removed.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def current_language
+    I18n.locale
+  end
+
   def view_mode_item(id, href, css, active)
     content_tag :a, content_tag(:i, '', class: "#{css} #{'icon-white' if active}"), {href: href, id: id, class: "btn #{'btn-success' if active}"}
   end
